78cf8a8366ad944cd58b1c57bb7b56be4d1801ed
Marlon Orellana <marlonorellana010@gmail.com>
2021-05-14 20:51:54 -0400

    Initial Commit

---
+++ SwiftUI-Planets/SwiftUI_PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlanetsApp.swift
+//  SwiftUI-Planets
+//
+//  Created by Marlon Orellana on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
